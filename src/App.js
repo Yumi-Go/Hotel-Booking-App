4ae62766565d4841ed23a8e2e58af6e3ab07c4f8
Yumi Go <yumigoyumi@gmail.com>
2024-01-11 15:29:37 +0000

    setting up, creating top menu bar and search bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Top from './components/header/Top'
+import Search from './components/header/Search'
+import Home from './components/pages/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Top/>
+      <Search/>
+          {/* <ul className="App-header">
+              <li>
+                  <Link to="/">Home</Link>
+              </li>
+              <li>
+                  <Link to="/top">
+                      Top Menu
+                  </Link>
+              </li>
+              <li>
+                  <Link to="/search">
+                      Search Bar
+                  </Link>
+              </li>
+          </ul> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/top" element={<Top />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+      </div>
   );
 }
 
