d441bca057cabefc6621f874ff506850b844492d
Yumi Go <yumigoyumi@gmail.com>
2024-01-22 00:23:53 +0000

    Seperation AuthContext.js from App.js

---
+++ src/AuthContext.js
@@ -0,0 +1,13 @@
+import React, { createContext, useContext } from 'react';
+import useAuth from './hooks/useAuth';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const { currentUser, logOut } = useAuth();
+    const value = { currentUser, logOut };
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export const useAuthContext = () => useContext(AuthContext);
