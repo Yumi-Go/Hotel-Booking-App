a1646ab35cb575e3d24dab2c67054103b20287f8
Yumi Go <yumigoyumi@gmail.com>
2024-01-22 00:23:53 +0000

    Seperation AuthContext.js from App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, createContext } from 'react';
+import React from 'react';
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Top from './components/header/Top/Top'
@@ -6,36 +6,24 @@ import Search from './components/header/Search/Search'
 import Home from './components/pages/Home'
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import useAuth from './hooks/useAuth';
+import { AuthProvider } from './AuthContext';
 
 
-
-function App() {
-
-  const { currentUser, logOut } = useAuth();
-  const AuthContext = createContext();
-  
-  useEffect(()=>{
-    console.log("Current User: ", currentUser);
-  }, [currentUser]);
-
-  const value = {currentUser, logOut}
+export default function App() {
 
   return (
-    <AuthContext.Provider value={value}>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <div className="App">
-          <Top/>
-          <Search/>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/top" element={<Top />} />
-            <Route path="/search" element={<Search />} />
-          </Routes>
-        </div>
-      </LocalizationProvider>
-    </AuthContext.Provider>
+      <AuthProvider>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <div className="App">
+            <Top/>
+            <Search/>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/top" element={<Top />} />
+              <Route path="/search" element={<Search />} />
+            </Routes>
+          </div>
+        </LocalizationProvider>
+      </AuthProvider>
   );
 }
-
-export default App;
