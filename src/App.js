9fcf5ff555a73f04df300619f4857fff168c537d
Yumi Go <yumigoyumi@gmail.com>
2024-02-13 02:16:40 +0000

    Fix: parent-child relation and props between Home, Search, App components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Top from './components/header/Top/Top'
@@ -10,23 +10,48 @@ import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { AuthProvider } from './AuthContext';
 import { Box } from '@mui/material';
+import { searchHotels } from './hooks/useHotelAPI';
 
 
 export default function App() {
+  const [searchResult, setSearchResult] = useState([]);
+  const [photos, setPhotos] = useState([]);
+  const [cityCode, setCityCode] = useState("PAR");
+  const [name, setName] = useState("WESTERN");
+  const [searchConditions, setSearchConditions] = useState({
+    adults: 2,
+    children: 0,
+    checkInDate: '2024-03-22',
+    checkOutDate: '2024-03-23',
+    roomQuantity: 1,
+    priceRange: "100-1500",
+    currency: "EUR"
+  });
+
+  const handleSearch = async () => {
+    try {
+      const response = await searchHotels(name, cityCode, searchConditions);
+      setSearchResult(response);
+      console.log("Updated searchResult in App:", response);
+    } catch (error) {
+      console.error("Error in handleSearch: ", error);
+      setSearchResult([]);
+    }
+  };
 
   return (
-      <AuthProvider>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <Box className="App">
-            <Top/>
-            <Search/>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/account" element={<Account />} />
-              <Route path="/history" element={<BookingHistory />} />
-            </Routes>
-          </Box>
-        </LocalizationProvider>
-      </AuthProvider>
+    <AuthProvider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Box className="App">
+          <Top />
+          <Search onSearch={handleSearch} />
+          <Routes>
+            <Route path="/" element={<Home searchResult={searchResult} photos={photos} />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/history" element={<BookingHistory />} />
+          </Routes>
+        </Box>
+      </LocalizationProvider>
+    </AuthProvider>
   );
 }
--- src/hooks/useHotelAPI.js
+++ src/hooks/useHotelAPI.js
@@ -1,4 +1,4 @@
-async function getData(url) {
+async function getToken() {
 
     const amadeusClientId = process.env.REACT_APP_AMADEUS_CLIENT_ID;
     const amadeusClientSecret = process.env.REACT_APP_AMADEUS_CLIENT_SECRET;
@@ -15,15 +15,26 @@ async function getData(url) {
         const authData = await authResponse.json();
         const token = authData.access_token;
         console.log("token:", token);
+        return token;
 
+    } catch (err) {
+        console.error("Error in searchRequest:", err);
+    }
+}
+
+
+
+async function getData(token, url) {
+    try {
         const response = await fetch(url, {
             method: "GET",
             headers: { Authorization: `Bearer ${token}` },
         });
-
-        const rawData = await response.json();
-        console.log("rawData: ", rawData.data);
-        return rawData.data;
+        console.log("response: ", response);
+        let rawData = await response.json();
+        rawData = await rawData.data;
+        console.log("rawData: ", rawData);
+        return rawData;
 
     } catch (err) {
         console.error("Error in searchRequest:", err);
@@ -31,7 +42,7 @@ async function getData(url) {
 }
 
 // Hotel List API
-async function getHotelIdByCity(cityCode) {
+async function getHotelIdByCity(token, cityCode) {
     try {
         const url =
         "https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city?" +
@@ -40,9 +51,10 @@ async function getHotelIdByCity(cityCode) {
         "radiusUnit=KM&" +
         "hotelSource=ALL";
         
-        const hotels = await getData(url);
+        const hotels = await getData(token, url);
 
         console.log("hotels: ", hotels);
+        
         const result = [];
 
         if (hotels && hotels.length) {
@@ -64,7 +76,7 @@ async function getHotelIdByCity(cityCode) {
 
 
 // Hotel Name Autocomplete API
-async function getHotelIdByName(name) {
+async function getHotelIdByName(token, name) {
     try {
         const url = 
         "https://test.api.amadeus.com/v1/reference-data/locations/hotel?" +
@@ -74,7 +86,7 @@ async function getHotelIdByName(name) {
         "lang=EN&" +
         "max=20";
         
-        const hotels = await getData(url);
+        const hotels = await getData(token, url);
 
         console.log("hotels: ", hotels);
         const result = [];
@@ -103,32 +115,110 @@ async function getHotelIdByName(name) {
 
 
 
-export async function getIds(name, cityCode) {
-    
-    let idsByName = [];
-    let idsByCity = [];
-    
-    if (name.length > 0) {
-        idsByName = await getHotelIdByName(name);
-        console.log("idsByName: ", idsByName);
-    }
-    if (cityCode.length > 0) {
-        idsByCity = await getHotelIdByCity(cityCode);
-        console.log("idsByCity: ", idsByCity);
+export async function getIds(token, name, cityCode) {
+
+
+    // Name Autocomplete API에서는 결과값이 20개까지만 나오고, 그 안에서도 뭐가 잘못된 건지 cityCode로 검색한거랑 겹치는게 하나도 없음. (e.g. 'CITADINES'으로 검색했을때 파리에 있는 CITADINES 호텔이 몇개 나옴에도 불구하고 PAR로 cityCode로 얻은 리스트에 존재하는 호텔 아이디가 하나도 발견이 안됨. 둘이 뭔가 문제가 있는듯..)
+    // 그래서 방법을 바꿈.. 일단은 Hotel List API만 쓰기로.. 아래처럼..
+    // cityCode로 검색 -> 그 결과 리스트 내에서 parameter name값이 포함된 호텔 검색 (e.g. parameter name이 'WESTERN'일때 name: "BEST WESTERN JARDIN DE CLUNY"이 찾아지는 것처럼..) 해서 호텔 아이디 리스트 추리기
+
+    try {
+        const url =
+        "https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city?" +
+        `cityCode=${cityCode}&` +
+        "radius=5&" +
+        "radiusUnit=KM&" +
+        "hotelSource=ALL";
+        
+        const hotelsData = await getData(token, url);
+
+        console.log("hotelsData: ", hotelsData);
+        
+        const result = [];
+
+        if (hotelsData && hotelsData.length) {
+            // result of below map will be array  e.g. ["TKSXRAHS", "VJPAR00S", ...]
+            hotelsData.forEach(async eachHotel => {
+                if (eachHotel.name.includes(name)) {
+                    result.push(eachHotel.hotelId);
+                    // result.push(eachHotel); // to check if the intersection result of cityCode & name is correct
+                }
+            });
+        }
+        else {
+            throw new Error(`No hotel data found with Hotel name ${name} in City Code ${cityCode}`);
+        }
+        console.log("result in getIds: ", result);
+        return result;
+    } catch (err) {
+        console.error("Error in getIds:", err);
+        throw err;
     }
 
-    let ids = [];
-    if (idsByName.length > 0 && idsByCity.length > 0) {
-        ids = idsByName.filter(async(id) => idsByCity.includes(id));
-    } else if (idsByName.length > 0) {
-        ids = idsByName;
-    } else if (idsByCity.length > 0) {
-        ids = idsByCity;
-    }    
-    ids = [...new Set(ids)];
 
-    console.log("ids in getIds: ", ids);
-    return ids;
+    
+    // // 원래 방법 2..
+    // let idsByNamePromise = name.length > 0 ? await getHotelIdByName(token, name) : Promise.resolve([]);
+    // let idsByCityPromise = cityCode.length > 0 ? await getHotelIdByCity(token, cityCode) : Promise.resolve([]);
+
+    // return Promise.all([idsByNamePromise, idsByCityPromise]).then(([idsByName, idsByCity]) => {
+    //     let ids = [];
+    //     if (idsByName.length > 0 && idsByCity.length > 0) {
+    //         console.log("idsByName: ", idsByName);
+    //         console.log("idsByCity: ", idsByCity);
+    //         // ids = idsByName.filter(id => idsByCity.includes(id));
+    //         ids = idsByName.filter(id => {
+    //             const isIncluded = idsByCity.includes(id);
+    //             console.log(`Checking ${id}: ${isIncluded}`);
+    //             return isIncluded;
+    //         });
+    //     } else if (idsByName.length > 0) {
+    //         ids = idsByName;
+    //     } else if (idsByCity.length > 0) {
+    //         ids = idsByCity;
+    //     }
+    //     ids = [...new Set(ids)]; // Ensure uniqueness
+
+    //     console.log("ids in getIds: ", ids);
+    //     return ids;
+    // });
+
+
+    // // 원래 방법 1..
+    // let idsByName = [];
+    // let idsByCity = [];
+    
+    // if (name.length > 0) {
+    //     idsByName = await getHotelIdByName(token, name);
+    //     // console.log("idsByName: ", idsByName);
+    // }
+    // if (cityCode.length > 0) {
+    //     idsByCity = await getHotelIdByCity(token, cityCode);
+    //     // console.log("idsByCity: ", idsByCity);
+    // }
+
+    // let ids = [];
+    // if (idsByName.length > 0 && idsByCity.length > 0) {
+    //     console.log("idsByName: ", idsByName);
+    //     console.log("idsByCity: ", idsByCity);
+
+    //     // ids = idsByName.filter(async(id) => idsByCity.includes(id));
+    //     ids = idsByName.filter(id => {
+    //         const isIncluded = idsByCity.includes(id);
+    //         console.log(`Checking ${id}: ${isIncluded}`);
+    //         return isIncluded;
+    //     }); // for check
+
+
+    // } else if (idsByName.length > 0) {
+    //     ids = idsByName;
+    // } else if (idsByCity.length > 0) {
+    //     ids = idsByCity;
+    // }    
+    // ids = [...new Set(ids)];
+
+    // console.log("ids in getIds: ", ids);
+    // return ids;
 }
 
 
@@ -138,29 +228,57 @@ export async function getIds(name, cityCode) {
 // get offers by searchConditions(except for hotel name)
 // each hotel with multiple offers
 // [{ ...hotel, photoUrls: [url1, url2..], offers: [{offer1},{offer2}..] }, { ...hotel, photoUrls: [url1, url2..], offers: [{offer1},{offer2}..] }, ...]
-export async function searchHotels(url) {
+export async function searchHotels(name, cityCode, searchConditions) {
     try {
-        const hotels = await getData(url);
+        const token = await getToken(); // get token only once only in this function
+        const ids = await getIds(token, name, cityCode);
+        // const hotelIds = ids.join("%2C"); // multiple hotels.. seperated by '%2C' like this.. e.g. hotelIds=MCLONGHM%2CHNPARKGU
+        // console.log("aggregated hotelIds: ", hotelIds);
+        console.log("ids: ", ids);
 
-        console.log("hotels: ", hotels);
 
-        if (hotels && hotels.length) {
+        // Hotel Search API free request quota 초과해서 현재 status: 429, statusText: "Too Many Requests" 응답이 옴. 매월 정해진 quota라 다음달까지 기다려야할듯.
+        // https://developers.amadeus.com/pricing#pricing-list 여기 참고
+        // 그 동안 다른 작업 하고있기.
+        const result = [];
+        ids.forEach(async(id) => {
+            // Hotel Search API
+            const url =
+            "https://test.api.amadeus.com/v3/shopping/hotel-offers?" +
+            `hotelIds=${id}&` +
+            `adults=${searchConditions.adults}&` +
+            `checkInDate=${searchConditions.checkInDate}&` +
+            `checkOutDate=${searchConditions.checkOutDate}&` +
+            `roomQuantity=${searchConditions.roomQuantity}&` +
+            `priceRange=${searchConditions.priceRange}&` +
+            `currency=${searchConditions.currency}&` +
+            "paymentPolicy=NONE&" +
+            "bestRateOnly=false";
 
-            const promises = hotels.map(async eachHotel => {
-                if (eachHotel.hotel && eachHotel.offers) {
-                    const photoUrls = await getPhotosByHotelName(eachHotel.hotel.name);
-                    return { ...eachHotel.hotel, photoUrls: photoUrls, offers: eachHotel.offers };
-                }
-            });
-            const result = await Promise.all(promises);
-            const filteredResults = result.filter(hotel => hotel !== undefined);
-            localStorage.setItem('searchResult', JSON.stringify(filteredResults));
-            return filteredResults;
-            
-        }
-        else {
-            throw new Error('No hotel data found');
-        }
+            const hotelData = await getData(token, url);
+            console.log("hotelData: ", hotelData);
+
+            let eachHotel = {};
+            if (hotelData && hotelData.length) {
+                console.log(`data of hotel id ${id}: `, hotelData[0]);
+                if (hotelData[0].hotel && hotelData[0].offers) {
+                    const photoUrls = await getPhotosByHotelName(hotelData[0].hotel.name);
+                    eachHotel = await { ...hotelData[0].hotel, photoUrls: photoUrls, offers: hotelData[0].offers };
+                    console.log("each Hotel with photos: ", eachHotel);
+                    result.push(eachHotel);
+                    localStorage.setItem('searchResult', JSON.stringify(result));
+
+                }                
+            }
+            // else {
+            //     throw new Error(`No hotel data found in hotel id '${id}'`);
+            // }
+        });
+        console.log("result: ", result);
+
+
+        return result;
+
     } catch (err) {
         console.error("Error in searchHotels:", err);
         throw err;
@@ -196,4 +314,6 @@ async function getPhotosByHotelName(hotelName) {
         throw error;
     }
 }
+
+
 
