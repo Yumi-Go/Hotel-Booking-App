1d81486d5f7678731718cd9d2da18f0b3f5a97ed
Yumi Go <yumigoyumi@gmail.com>
2024-02-19 16:47:53 +0000

    Seperate components in HotelDetail.js

---
+++ src/components/pages/Hotel/Availability.js
@@ -0,0 +1,90 @@
+import { useState, useEffect } from "react";
+import { styled } from '@mui/material/styles';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
+
+export default function Availability({ hotelObj }) {
+    console.log("Received hotelObj in Availability: ", hotelObj);
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    useEffect(() => {
+        const rooms = () => {
+            for (const [key, value] of Object.entries(hotelObj.offers[0].room)) {
+                if (key === "typeEstimated" || key === "description") {
+                    console.log(`*** ${key}:`);
+                    for (const [k, v] of Object.entries(value)) {
+                        console.log(`${k}: ${v}`);
+                    }
+                }  
+            }
+        };
+        rooms();
+    }, [hotelObj]);
+
+
+    return (
+        <Grid xs={6} md={8} sx={{ py: 2, px: 0, mx: 0, backgroundColor: 'orange' }}>
+            <Item sx={{ boxShadow: 0 }}>
+                <Stack
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={2}
+                >
+                    {hotelObj.offers.map((offerObj, index) => (
+                        <Item key={index} sx={{ width: '95%', border: 1 }}>
+                            <Box sx={{ textAlign: 'left' }}>
+                                Room Type: {offerObj.room.typeEstimated.category}
+                            </Box>
+                            <Box sx={{ textAlign: 'left' }}>
+                                Beds: {offerObj.room.typeEstimated.bedType} {offerObj.room.typeEstimated.beds}
+                            </Box>
+                            {/* <Box sx={{ textAlign: 'left' }}>
+                                Description: {offerObj.room.description.text}
+                            </Box> */}
+                            <Box sx={{ textAlign: 'left' }}>
+                                Price: {offerObj.price.total} {offerObj.price.currency}
+                            </Box>
+                        </Item>
+                    ))}
+                </Stack>
+            </Item>
+        </Grid>
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/components/pages/Hotel/HotelDetail.js
+++ src/components/pages/Hotel/HotelDetail.js
@@ -13,6 +13,10 @@ import InfoIcon from '@mui/icons-material/Info';
 import CircularProgress from '@mui/material/CircularProgress';
 import Typography from '@mui/material/Typography';
 import LinearProgress from '@mui/material/LinearProgress';
+import Availability from "./Availability";
+import Ratings from "./Ratings";
+import Photos from "./Photos";
+import Map from "./Map";
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -70,7 +74,6 @@ export default function HotelDetail({ hotelObj, ratings }) { // make hotelObj, r
     console.log("Received hotelObj: ", hotelObj);
     console.log("Received ratings: ", ratings);
 
-
     // // for testing getPhotosByHotelName.. remove this later
     // const [photos, setPhotos] = useState([]);
 
@@ -86,7 +89,6 @@ export default function HotelDetail({ hotelObj, ratings }) { // make hotelObj, r
     //     };
     //     fetchPhotos();
     // }, [hotelObj.hotel.name]);
-
 
     useEffect(() => {
         const rooms = () => {
@@ -105,106 +107,19 @@ export default function HotelDetail({ hotelObj, ratings }) { // make hotelObj, r
 
   return (
     <Box sx={{ width: "100%", height: "100%", display:"flex", position: "absolute", justifyContent: 'center'}}>
-      <Stack sx={{ width: "100%", alignItems: 'center', bgcolor: '#dedede', margin: 0, padding: 0 }}>
-        <Box>
-            <h1>
-                {hotelObj.hotel.name}
-            </h1>
-        </Box>
-        <Box sx={{ width: "95%", my: "20px", bgcolor: "red" }}>
-            <Grid container spacing={0} sx={{ width: '100%'}}>
-                <Grid xs={6} md={7} sx={{ height: 500, py: 2, px: 0, mx: 0 }}>
-                    <Item sx={{ height: "100%", padding: 0, margin: 0, bgcolor: "pink"}}>
-                        <ImageList sx={{ width: "100%", height: "100%", margin: 0, padding: 0 }}>
-                            {/* <ImageListItem key="Subheader" cols={2}>
-                                <ListSubheader component="div">Photos</ListSubheader>
-                            </ImageListItem> */}
-                            {Object.values(hotelObj.photoUrls)[0].map((url, index) => (
-                                <ImageListItem key={index}>
-                                    <img
-                                        src={url}
-                                        alt={index}
-                                        loading="lazy"
-                                    />
-                                    <ImageListItemBar
-                                        title={hotelObj.hotel.name}
-                                        actionIcon={
-                                            <IconButton
-                                                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                                                aria-label={`info about ${hotelObj.hotel.name}`}
-                                            >
-                                                <InfoIcon />
-                                            </IconButton>
-                                        }
-                                    />
-                                </ImageListItem>
-                            ))}
-                        </ImageList>
-                    </Item>
+        <Stack sx={{ width: "100%", alignItems: 'center', bgcolor: '#dedede', margin: 0, padding: 0 }}>
+            <Box>
+                <h1>{hotelObj.hotel.name}</h1>
+            </Box>
+            <Box sx={{ width: "95%", my: "20px", bgcolor: "red" }}>
+                <Grid container spacing={0} sx={{ width: '100%'}}>
+                    <Photos hotelObj={hotelObj} />
+                    <Map hotelObj={hotelObj} />
+                    <Availability hotelObj={hotelObj} />
+                    <Ratings ratings={ratings} />
                 </Grid>
-                <Grid xs={6} md={5} sx={{ py: 2, px: 0, mx: 0, backgroundColor: 'blue' }}>
-                    <Item sx={{ height: '100%', bgcolor: "pink", padding: 0 }}>
-                        <iframe
-                            src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_FIREBASE_API_KEY}&q=place_id:ChIJZcFL1TMFdkgRZcciwfX0gr0`}
-                            style={{ width: "100%", height: "100%", border: "0" }}
-                            title="map"
-                            allowFullScreen
-                        />
-                    </Item>
-                </Grid>
-                <Grid xs={6} md={8} sx={{ py: 2, px: 0, mx: 0, backgroundColor: 'orange' }}>
-                    <Item sx={{ boxShadow: 0 }}>
-                        <Stack
-                            direction="column"
-                            justifyContent="center"
-                            alignItems="center"
-                            spacing={2}
-                        >
-                            {hotelObj.offers.map((offerObj, index) => (
-                                <Item key={index} sx={{ width: '95%', border: 1 }}>
-                                    <Box sx={{ textAlign: 'left' }}>
-                                        Room Type: {offerObj.room.typeEstimated.category}
-                                    </Box>
-                                    <Box sx={{ textAlign: 'left' }}>
-                                        Beds: {offerObj.room.typeEstimated.bedType} {offerObj.room.typeEstimated.beds}
-                                    </Box>
-                                    <Box sx={{ textAlign: 'left' }}>
-                                        Description: {offerObj.room.description.text}
-                                    </Box>
-                                    <Box sx={{ textAlign: 'left' }}>
-                                        Price: {offerObj.price.total} {offerObj.price.currency}
-                                    </Box>
-                                </Item>
-                            ))}
-                        </Stack>
-                    </Item>
-                </Grid>
-                {ratings ? (
-                <Grid xs={6} md={4} sx={{ py: 2, px: 0, mx: 0, backgroundColor: 'green' }}>
-                    <Item sx={{ boxShadow: 0, border: 1}}>
-                        <h3>Overall</h3>
-                        [{ratings.numberOfReviews} reviews / {ratings.numberOfRatings} ratings]
-                        <LinearProgressWithLabel variant="determinate" value={ratings.overallRating} />
-                    </Item>
-                    <Item sx={{ boxShadow: 0, border: 1, backgroundColor: "purple"}}>
-                        <Grid container justifyContent="center" alignItems="center" spacing={1} sx={{ margin: 0, backgroundColor: "yellow" }}>
-                            {Object.entries(ratings.sentiments).map(([key, value], index) => (
-                                <Grid item key={key} xs={2} sm={2} md={5} justifyContent="center">
-                                    <Item sx={{ boxShadow: 0, border: 1 }}>
-                                        <div>
-                                            <div>{key}</div>
-                                            <div><CircularProgressWithLabel value={value} /></div>
-                                        </div>
-                                    </Item>
-                                </Grid>
-                            ))}
-                        </Grid>
-                    </Item>
-                </Grid>
-                ) : <div>Loading ratings...</div>}
-            </Grid>
-        </Box>
-      </Stack>
+            </Box>
+        </Stack>
     </Box>
   );
 }
