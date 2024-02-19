201ba9c368d315adc7fc03dbb444a03cdadbaf86
Yumi Go <yumigoyumi@gmail.com>
2024-02-19 15:45:38 +0000

    Displaying Room Availability in HotelDetail page succeed!

--- src/components/pages/Hotel/HotelDetail.js
+++ src/components/pages/Hotel/HotelDetail.js
@@ -64,9 +64,43 @@ const Item = styled(Paper)(({ theme }) => ({
   }
 
 
+
+// 나중에 컴포넌트 좀 분리하기!
 export default function HotelDetail({ hotelObj, ratings }) { // make hotelObj, ratings single parameter merged later...
     console.log("Received hotelObj: ", hotelObj);
     console.log("Received ratings: ", ratings);
+
+
+    // // for testing getPhotosByHotelName.. remove this later
+    // const [photos, setPhotos] = useState([]);
+
+    // useEffect(() => {
+    //     const fetchPhotos = async () => {
+    //         try {
+    //             const photoUrls = await getPhotosByHotelName(hotelObj.hotel.name);
+    //             setPhotos(photoUrls);
+    //             // console.log("photoUrls: ", photoUrls);
+    //         } catch (error) {
+    //             console.error("Failed to fetch photos:", error);
+    //         }
+    //     };
+    //     fetchPhotos();
+    // }, [hotelObj.hotel.name]);
+
+
+    useEffect(() => {
+        const rooms = () => {
+            for (const [key, value] of Object.entries(hotelObj.offers[0].room)) {
+                if (key === "typeEstimated" || key === "description") {
+                    console.log(`*** ${key}:`);
+                    for (const [k, v] of Object.entries(value)) {
+                        console.log(`${k}: ${v}`);
+                    }
+                }  
+            }
+        };
+        rooms();
+    }, [hotelObj]);
 
 
   return (
@@ -78,9 +112,9 @@ export default function HotelDetail({ hotelObj, ratings }) { // make hotelObj, r
             </h1>
         </Box>
         <Box sx={{ width: "95%", my: "20px", bgcolor: "red" }}>
-            <Grid container spacing={2}>
-                <Grid xs={6} md={7} sx={{ height: 500, padding: 2 }}>
-                    <Item sx={{ height: "100%", bgcolor: "pink"}}>
+            <Grid container spacing={0} sx={{ width: '100%'}}>
+                <Grid xs={6} md={7} sx={{ height: 500, py: 2, px: 0, mx: 0 }}>
+                    <Item sx={{ height: "100%", padding: 0, margin: 0, bgcolor: "pink"}}>
                         <ImageList sx={{ width: "100%", height: "100%", margin: 0, padding: 0 }}>
                             {/* <ImageListItem key="Subheader" cols={2}>
                                 <ListSubheader component="div">Photos</ListSubheader>
@@ -108,8 +142,8 @@ export default function HotelDetail({ hotelObj, ratings }) { // make hotelObj, r
                         </ImageList>
                     </Item>
                 </Grid>
-                <Grid xs={6} md={5} sx={{ padding: 2 }}>
-                    <Item sx={{ height: '100%', bgcolor: "pink" }}>
+                <Grid xs={6} md={5} sx={{ py: 2, px: 0, mx: 0, backgroundColor: 'blue' }}>
+                    <Item sx={{ height: '100%', bgcolor: "pink", padding: 0 }}>
                         <iframe
                             src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_FIREBASE_API_KEY}&q=place_id:ChIJZcFL1TMFdkgRZcciwfX0gr0`}
                             style={{ width: "100%", height: "100%", border: "0" }}
@@ -118,54 +152,56 @@ export default function HotelDetail({ hotelObj, ratings }) { // make hotelObj, r
                         />
                     </Item>
                 </Grid>
-                <Grid xs={6} md={4}>
-                    <Item>Available roome list here!!</Item>
-                </Grid>
-                <Grid xs={6} md={8}>
-                    <Item>something else...</Item>
-                </Grid>
-
-                <Grid xs={12} md={12}>
-                    <Item>
-                        {ratings ? (
-                            <Stack
-                                direction="column"
-                                justifyContent="center"
-                                alignItems="center"
-                            >
-
-                                <Item sx={{ width: '80%', boxShadow: 0 }}>
-                                    <h3>Overall</h3>
-                                    [{ratings.numberOfReviews} reviews / {ratings.numberOfRatings} ratings]
-                                    <LinearProgressWithLabel variant="determinate" value={ratings.overallRating} />
+                <Grid xs={6} md={8} sx={{ py: 2, px: 0, mx: 0, backgroundColor: 'orange' }}>
+                    <Item sx={{ boxShadow: 0 }}>
+                        <Stack
+                            direction="column"
+                            justifyContent="center"
+                            alignItems="center"
+                            spacing={2}
+                        >
+                            {hotelObj.offers.map((offerObj, index) => (
+                                <Item key={index} sx={{ width: '95%', border: 1 }}>
+                                    <Box sx={{ textAlign: 'left' }}>
+                                        Room Type: {offerObj.room.typeEstimated.category}
+                                    </Box>
+                                    <Box sx={{ textAlign: 'left' }}>
+                                        Beds: {offerObj.room.typeEstimated.bedType} {offerObj.room.typeEstimated.beds}
+                                    </Box>
+                                    <Box sx={{ textAlign: 'left' }}>
+                                        Description: {offerObj.room.description.text}
+                                    </Box>
+                                    <Box sx={{ textAlign: 'left' }}>
+                                        Price: {offerObj.price.total} {offerObj.price.currency}
+                                    </Box>
                                 </Item>
-
-                                {Object.entries(ratings)
-                                .map(([key, value]) => {
-                                    if (key === 'sentiments') {
-                                        return (
-                                            <Item key={key} sx={{ boxShadow: 0 }} alignItems="center">
-                                                {/* <div>Sentiments:</div> */}
-                                                <Stack direction="row" spacing={2} >
-                                                    {Object.entries(value).map(([k, v]) => (
-                                                        <Item key={k} sx={{ width: '100px', boxShadow: 0, border: 1 }}>
-                                                            <div>
-                                                                <div>{k}</div>
-                                                                <div><CircularProgressWithLabel value={v} /></div>
-                                                            </div>
-                                                        </Item>
-                                                    ))}
-                                                </Stack>  
-                                            </Item>
-                                        );
-                                    } else {
-                                        return "";
-                                    }
-                                })}
-                            </Stack>
-                        ) : <div>Loading ratings...</div>}
+                            ))}
+                        </Stack>
                     </Item>
                 </Grid>
+                {ratings ? (
+                <Grid xs={6} md={4} sx={{ py: 2, px: 0, mx: 0, backgroundColor: 'green' }}>
+                    <Item sx={{ boxShadow: 0, border: 1}}>
+                        <h3>Overall</h3>
+                        [{ratings.numberOfReviews} reviews / {ratings.numberOfRatings} ratings]
+                        <LinearProgressWithLabel variant="determinate" value={ratings.overallRating} />
+                    </Item>
+                    <Item sx={{ boxShadow: 0, border: 1, backgroundColor: "purple"}}>
+                        <Grid container justifyContent="center" alignItems="center" spacing={1} sx={{ margin: 0, backgroundColor: "yellow" }}>
+                            {Object.entries(ratings.sentiments).map(([key, value], index) => (
+                                <Grid item key={key} xs={2} sm={2} md={5} justifyContent="center">
+                                    <Item sx={{ boxShadow: 0, border: 1 }}>
+                                        <div>
+                                            <div>{key}</div>
+                                            <div><CircularProgressWithLabel value={value} /></div>
+                                        </div>
+                                    </Item>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Item>
+                </Grid>
+                ) : <div>Loading ratings...</div>}
             </Grid>
         </Box>
       </Stack>
