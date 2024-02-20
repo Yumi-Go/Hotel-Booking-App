e65d118cb0d6e43edc608e97fbd1617e6eaa81ee
Yumi Go <yumigoyumi@gmail.com>
2024-02-20 11:32:43 +0000

    backup

---
+++ src/hooks/useFormat.js
@@ -0,0 +1,51 @@
+export function capitalize(string) {
+    if (string) {
+        const firstLetter = string[0].toUpperCase();
+        const rest = string.slice(1).toLowerCase();
+        return firstLetter + rest;
+    } else {
+        return string;
+    }
+}
+
+export function capitalizeWords(paragraph) {
+    if (!paragraph) return paragraph;
+    return paragraph
+        .split(' ')
+        .map(word => {
+            if (word) {
+                const firstLetter = word[0].toUpperCase();
+                const rest = word.slice(1).toLowerCase();
+                return firstLetter + rest;
+            } else {
+                return word;
+            }
+        })
+        .join(' ');
+}
+
+
+
+export function formatDescription(text) {
+    if (!text) return [];
+    return text.replace(/\n/g, ' ').split(', ');
+}
+
+
+export function extractSquareMeters(text) {
+    const pattern = /\b(\d+)sqm/g;
+    let results = [];
+    let match;
+    
+    while ((match = pattern.exec(text)) !== null) {
+      results.push(parseInt(match[1], 10));
+    }
+    
+    return results;
+  }
+
+
+
+export function formatFullName(fName, mName, lName) {
+    return `${capitalize(fName)} ${capitalize(mName)} ${capitalize(lName)}`;
+}
